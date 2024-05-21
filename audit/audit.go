ea02b4ce26fe3f5b60ae7ed696d8662465379c03
John Doak <jdoak@ElephantInTheRoom.local>
2024-05-20 17:39:27 -0700

    Adding smart client and go mod files

---
+++ audit/audit.go
@@ -0,0 +1,271 @@
+/*
+Package audit implements a smart client to a remote audit server. This handles any connection problems silently,
+while providing ways to detect problems through whatever alerting method is desired. It also handles an exponential
+backoff when trying to reconnect to the remote audit server as to prevent any overwhelming of the remote audit server.
+
+This is the preferred way to connect to a remote audit server. There is a more low-level way to connect to a remote
+audit server in the base package. This one allows for you to completely customize how you want to handle issues,
+but it is more work to do so.
+
+Example using the domainsocket package:
+
+	// Create a function that will create a new connection to the remote audit server.
+	// We use this function to create a new connection when the connection is broken.
+	cc := func() (conn.Audit, error) {
+		return conn.NewDomainSocket()
+	}
+
+	c, err := audit.New(cc)
+	if err != nil {
+		// Handle error.
+	}
+	defer c.Close(context.Background())
+
+	// This is optional if you want to get notifications of logging problems.
+	go func() {
+		for notifyMsg := range c.Notify() {
+			// Handle error notification.
+			// You can log them or whatever you want to do.
+		}
+	}()
+
+	// Send a message to the remote audit server.
+	if err := c.Send(context.Background(), msgs.Msg{<add record information>}); err != nil {
+		// Handle error.
+		// Errors here will either be:
+		// 1. base.ErrValidation , which means your message is invalid.
+		// 2. A standard error, which means there is an error condition we haven't categorized yet.
+	}
+*/
+package audit
+
+import (
+	"context"
+	"fmt"
+	"reflect"
+	"runtime"
+	"sync/atomic"
+	"testing"
+	"time"
+
+	"element-of-surprise/audit/audit/base"
+	"element-of-surprise/audit/audit/conn"
+	"element-of-surprise/audit/audit/msgs"
+
+	"github.com/Azure/retry/exponential"
+)
+
+// NotifyError is an error that is sent to the Notify channel when the connection to the remote audit server is broken.
+type NotifyError struct {
+	// Time is the time the error occurred.
+	Time time.Time
+	// Err is the error that happened. This will be a an base.ErrConnection or base.ErrQueueFull .
+	Err base.Error
+}
+
+// Client is an audit server smart client. This handles any connection problems silently, while providing ways to
+// detect problems through whatever alerting method is desired.
+type Client struct {
+	// client holds the current audit client. If this is nil, it means we are trying to get a new client.
+	// When that occurs, we will do a noop on Send().
+	client *base.Client
+	// replaceClient is used to ensure we only replace the client once. This is needed because we can have multiple
+	// goroutines trying to replace the client at the same time.
+	replacingClient atomic.Bool
+
+	// create is the function that creates a new connection to the remote audit server. This is used when the
+	// connection is broken.
+	create CreateConn
+	// backoff is the exponential backoff for making a new connection to the remote audit server.
+	backoff *exponential.Backoff
+	policy  exponential.Policy
+
+	// notifier is the channel that will receive errors when the connection to the remote audit server is broken or
+	// the queue is full. If this channel is full, the error will be dropped.
+	notifier chan NotifyError
+	// auditOptions are the options for the underlying base.Client.
+	auditOptions []base.Option
+
+	// replaceBackoffRunner is a function that replaces the audit client with a new one using an exponential backoff.
+	// In production, this uses .replaceBackoff. In testing this can be replaced with a function that does nothing.
+	replaceBackoffRunner func(ctx context.Context) error
+	// sendRunner is a function that sends a message to the remote audit server.
+	// In production, this uses .client.Sent(). In testing this can be replaced with a function that does nothing.
+	sendRunner func(ctx context.Context, msg msgs.Msg) error
+}
+
+// CreateConn is a function that creates a connection to a remote audit server.
+type CreateConn func() (conn.Audit, error)
+
+// Option is an option for the smart client.
+type Option func(*Client) error
+
+// WithAuditOptions sets the audit options for the underlying audit client.
+func WithAuditOptions(options ...base.Option) Option {
+	return func(c *Client) error {
+		c.auditOptions = options
+		return nil
+	}
+}
+
+// WithBackoffPolicy sets the exponential backoff policy for making a new connection to the remote audit server.
+// By default this uses the default backoff settings from the exponential package.
+func WithExponentialBackoff(policy exponential.Policy) Option {
+	return func(c *Client) error {
+		c.policy = policy
+		return nil
+	}
+}
+
+// New creates a new smart client to the remote audit server. You can get a CreateConn
+// from conn.NewDomainSocket(), conn.NewTCP() or conn.NewNoop(). The last one is useful for testing
+// when you don't want to send logs anywhere.
+func New(create CreateConn, options ...Option) (*Client, error) {
+	connect, err := create()
+	if err != nil {
+		return nil, err
+	}
+
+	if runtime.GOOS != "linux" && !testing.Testing() {
+		if connect.Type() != conn.TypeNoOP {
+			return nil, fmt.Errorf("audit: only linux is clients can use Audit with non-NoOp connections")
+		}
+	}
+
+	c := &Client{
+		create:   create,
+		notifier: make(chan NotifyError, 1),
+	}
+	c.replaceBackoffRunner = c.replaceBackoff
+	c.replacingClient.Store(false)
+
+	for _, o := range options {
+		o(c)
+	}
+
+	if reflect.ValueOf(c.policy).IsZero() {
+		var err error
+		c.backoff, err = exponential.New()
+		if err != nil {
+			return nil, err
+		}
+	} else {
+		c.backoff, err = exponential.New(exponential.WithPolicy(c.policy))
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	// Create the audit client.
+	aClient, err := base.New(connect, c.auditOptions...)
+	if err != nil {
+		return nil, err
+	}
+	c.client = aClient
+	c.sendRunner = c.client.Send
+
+	return c, nil
+}
+
+// Notify returns a channel that will receive errors when the connection to the remote audit server is broken or
+// the queue is full. If this channel is full, the error will be dropped.
+func (c *Client) Notify() <-chan NotifyError {
+	return c.notifier
+}
+
+// Send sends a message to the remote audit server. If the connection is broken, it will attempt to reconnect
+// but you will not receive an error. The only errors that will be returned are due to the Record being
+// invalid, trying to send a Msg with a type not DataPlane or ControlPlane or when we receive an
+// uncategorized error (which always indicates a handling bug in the Client).
+// Context timeouts will cause the write to drop, but not an error as all sends are
+// non-blocking. As a rule, you should not use Context timeouts as this is async. As a best practice,
+// you should do client.Send(context.WithoutCancel(ctx), msg) to prevent timeouts.
+func (c *Client) Send(ctx context.Context, msg msgs.Msg) error {
+	if err := c.sendRunner(ctx, msg); err != nil {
+		if base.IsUnrecoverable(err) {
+			c.replaceBackoffRunner(ctx) // Dropping error on purpose
+			return nil
+		}
+		if base.IsQueueFull(err) {
+			c.sendNotification(base.Error{Err: err, Category: base.ErrQueueFull})
+			return err
+		}
+		if base.IsValidationErr(err) {
+			return err
+		}
+		return fmt.Errorf("bug: error sending audit record that is uncategorized: %w", err)
+	}
+	return nil
+}
+
+// Close closes the connection to the remote audit server.
+func (c *Client) Close(ctx context.Context) error {
+	defer close(c.notifier)
+
+	// We do a noop if the client is nil, indicating we are trying to get a new client.
+	if c.client == nil {
+		return nil
+	}
+	return c.client.Close(ctx)
+}
+
+// replaceBackoff replaces the audit client with a new one using an exponential backoff.
+func (c *Client) replaceBackoff(ctx context.Context) error {
+	ctx = context.WithoutCancel(ctx)
+	return c.backoff.Retry(
+		ctx,
+		func(ctx context.Context, r exponential.Record) error {
+			c.client.Logger().Info(fmt.Sprintf("replacing audit client connection: Attempt %d, wait interval %v", r.Attempt, r.LastInterval))
+			return c.replace(ctx)
+		},
+	)
+}
+
+// replace replaces the audit client with a new one.
+func (c *Client) replace(ctx context.Context) error {
+	swapped := c.replacingClient.CompareAndSwap(false, true)
+
+	// This indicates that we are already replacing the client, so we don't need to do anything.
+	if !swapped {
+		return nil
+	}
+
+	// Indicate we are done changing out the client.
+	defer c.replacingClient.Store(false)
+
+	// Create a new connection.
+	newConn, err := c.create()
+	if err != nil {
+		c.sendNotification(base.Error{Err: fmt.Errorf("error while trying to make a new connection: %w", err), Category: base.ErrConnection})
+		return err
+	}
+
+	// Reset the client with the new connection.
+	if err := c.client.Reset(ctx, newConn); err != nil {
+		c.client.Logger().Error(fmt.Sprintf("error while trying to make a new client: %v", err))
+		c.sendNotification(base.Error{Err: fmt.Errorf("error while trying to make a new client: %w", err), Category: base.ErrConnection})
+		// This should nevever happen, because the only thing that causes an error is that create() is returning
+		// nil. In that case, the error is permanent and we should cancel retries.
+		return fmt.Errorf("%w: %w", err, exponential.ErrPermanent)
+	}
+	return nil
+}
+
+// sendNotification sends a notification to the notifier channel. If the channel is full, the notification is dropped.
+func (c *Client) sendNotification(err base.Error) {
+	if err.Err == nil {
+		return
+	}
+
+	notice := NotifyError{
+		Err:  err,
+		Time: time.Now().UTC(),
+	}
+
+	select {
+	case c.notifier <- notice:
+	// Do nothing.
+	default:
+		// We don't care if the notifier is full.
+	}
+}
---
+++ audit/audit_test.go
@@ -0,0 +1,359 @@
+package audit
+
+import (
+	"context"
+	"errors"
+	"sync"
+	"sync/atomic"
+	"testing"
+	"time"
+
+	"element-of-surprise/audit/audit/base"
+	"element-of-surprise/audit/audit/conn"
+	"element-of-surprise/audit/audit/msgs"
+)
+
+func TestSend(t *testing.T) {
+	t.Parallel()
+
+	replaceRan := false
+
+	tests := []struct {
+		name             string
+		client           *Client
+		baseClient       *base.Client
+		wantRanReplace   bool
+		wantNotification bool
+		err              bool
+	}{
+		{
+			name: "Error: IsUnrecoverable()",
+			client: &Client{
+				sendRunner: func(ctx context.Context, msg msgs.Msg) error {
+					return base.Error{Err: errors.New("error"), Category: base.ErrConnection}
+				},
+				replaceBackoffRunner: func(ctx context.Context) error {
+					replaceRan = true
+					return nil
+				},
+			},
+			wantRanReplace: true,
+		},
+		{
+			name: "Error: IsQueueFull()",
+			client: &Client{
+				sendRunner: func(ctx context.Context, msg msgs.Msg) error {
+					return base.Error{Err: errors.New("error"), Category: base.ErrQueueFull}
+				},
+				notifier: make(chan NotifyError, 1),
+			},
+			wantNotification: true,
+			err:              true,
+		},
+		{
+			name: "Error: IsValidationErr()",
+			client: &Client{
+				sendRunner: func(ctx context.Context, msg msgs.Msg) error {
+					return base.Error{Err: errors.New("error"), Category: base.ErrValidation}
+				},
+			},
+			err: true,
+		},
+		{
+			name: "Error: error is uncategorized",
+			client: &Client{
+				sendRunner: func(ctx context.Context, msg msgs.Msg) error {
+					return errors.New("error")
+				},
+			},
+			err: true,
+		},
+		{
+			name: "Success",
+			client: &Client{
+				sendRunner: func(ctx context.Context, msg msgs.Msg) error {
+					return nil
+				},
+			},
+		},
+	}
+
+	for _, test := range tests {
+		replaceRan = false
+
+		err := test.client.Send(context.Background(), msgs.Msg{})
+		switch {
+		case test.err && err == nil:
+			t.Errorf("TestSend(%s): got err == nil, want err != nil", test.name)
+			continue
+		case !test.err && err != nil:
+			t.Errorf("TestSend(%s): got err == %s, want err == nil", test.name, err)
+			continue
+		case err != nil:
+			continue
+		}
+
+		if replaceRan != test.wantRanReplace {
+			t.Errorf("TestSend(%s): got replaceRan == %t, want replaceRan == %t", test.name, replaceRan, test.wantRanReplace)
+		}
+		if test.wantNotification {
+			<-test.client.Notify() // If we don't get a notification, the test will fail.
+		}
+	}
+}
+
+func TestClose(t *testing.T) {
+	t.Parallel()
+
+	clientToClose, err := base.New(conn.NewNoOP())
+	if err != nil {
+		panic(err)
+	}
+
+	tests := []struct {
+		name   string
+		client *Client
+	}{
+		{
+			name: "Success with nil .client",
+			client: &Client{
+				notifier: make(chan NotifyError),
+			},
+		},
+		{
+			name: "Success with non-nil .client",
+			client: &Client{
+				client:   clientToClose,
+				notifier: make(chan NotifyError),
+			},
+		},
+	}
+
+	for _, test := range tests {
+		err := test.client.Close(context.Background())
+		if err != nil {
+			t.Errorf("TestClose(%s): got err == %s", test.name, err)
+		}
+		<-test.client.Notify() // Make sure the notifer is closed.
+	}
+}
+
+type fakeAudit struct {
+	id int
+	conn.Audit
+}
+
+func TestReplace(t *testing.T) {
+	t.Parallel()
+
+	bc, err := base.New(fakeAudit{id: 0, Audit: conn.NewNoOP()})
+	if err != nil {
+		panic(err)
+	}
+
+	tests := []struct {
+		name            string
+		replacingClient bool
+		baseClient      *base.Client
+		create          func() (conn.Audit, error)
+		wantFakeAuditID int
+		err             bool
+	}{
+		{
+			name:            "Client is already being replaced",
+			replacingClient: true,
+			baseClient:      bc,
+			wantFakeAuditID: 0,
+		},
+		{
+			name: "CreateConn fails",
+			create: func() (conn.Audit, error) {
+				return nil, errors.New("error")
+			},
+			baseClient: &base.Client{},
+			err:        true,
+		},
+		{
+			name: "base.Client.Reset() fails",
+			create: func() (conn.Audit, error) {
+				return fakeAudit{id: 1, Audit: conn.NewNoOP()}, nil
+			},
+			baseClient: nil, // This causes an automatic failure
+			err:        true,
+		},
+		{
+			name: "Success",
+			create: func() (conn.Audit, error) {
+				return fakeAudit{id: 1, Audit: conn.NewNoOP()}, nil
+			},
+			baseClient:      bc,
+			wantFakeAuditID: 1,
+		},
+	}
+
+	for _, test := range tests {
+		client := &Client{create: test.create, client: test.baseClient}
+		client.replacingClient.Store(test.replacingClient)
+
+		err := client.replace(context.Background())
+		switch {
+		case test.err && err == nil:
+			t.Errorf("TestReplace(%s): got err == nil, want err != nil", test.name)
+			<-client.Notify() // Make sure the error is sent to the notifier.
+			continue
+		case !test.err && err != nil:
+			t.Errorf("TestReplace(%s): got err == %s, want err == nil", test.name, err)
+			continue
+		case err != nil:
+			continue
+		}
+
+		if client.client.Conn().(fakeAudit).id != test.wantFakeAuditID {
+			t.Errorf("TestReplace(%s): got fakeAudit.id == %d, want fakeAudit.id == %d", test.name, client.client.Conn().(fakeAudit).id, test.wantFakeAuditID)
+		}
+	}
+}
+
+func TestSendNotification(t *testing.T) {
+	t.Parallel()
+
+	// Send without an error.
+	client := Client{notifier: make(chan NotifyError, 1)}
+	client.sendNotification(base.Error{})
+	select {
+	case <-client.notifier:
+		t.Fatalf("TestSendNotification(sent NotifyError with no error): got NotifyError, expected message drop")
+	default:
+	}
+
+	// Send with room in channel.
+	client = Client{notifier: make(chan NotifyError, 1)}
+	client.sendNotification(base.Error{Err: errors.New("error"), Category: base.ErrQueueFull})
+	select {
+	case notice := <-client.notifier:
+		if notice.Time.IsZero() {
+			t.Fatalf("TestSendNotification(sent NotifyError with error): got Time zero, expected non-zero")
+		}
+		if notice.Err.Err == nil {
+			t.Fatalf("TestSendNotification(sent NotifyError with error): got NotifyError.Err.Err == nil, expected non-nil")
+		}
+	default:
+		t.Fatalf("TestSendNotification(sent NotifyError with error): got no NotifyError, expected NotifyError")
+	}
+
+	// Send with no room in channel.
+	client = Client{notifier: make(chan NotifyError)}
+	client.sendNotification(base.Error{Err: errors.New("error"), Category: base.ErrQueueFull})
+	select {
+	case <-client.notifier:
+		t.Fatalf("TestSendNotification(sent NotifyError with error, but no room in queue): got NotifyError, expected message drop")
+	default:
+	}
+}
+
+type auditMsgCollector struct {
+	conn.Audit
+
+	recMsgs []msgs.Msg
+	hbMsgs  []msgs.Msg
+	mu      sync.Mutex
+
+	num      atomic.Uint64
+	errAfter int
+}
+
+func (a *auditMsgCollector) Type() conn.Type {
+	return conn.TypeDomainSocket
+}
+
+func (a *auditMsgCollector) collected(i int) {
+	for {
+		a.mu.Lock()
+		if len(a.recMsgs) >= i {
+			a.mu.Unlock()
+			return
+		}
+		a.mu.Unlock()
+		time.Sleep(100 * time.Nanosecond)
+	}
+}
+
+func (a *auditMsgCollector) Write(ctx context.Context, msg msgs.Msg) error {
+	if n := a.num.Add(1); a.errAfter > 0 && n > uint64(a.errAfter) {
+		return errors.New("error after")
+	}
+
+	a.mu.Lock()
+	defer a.mu.Unlock()
+
+	switch msg.Type {
+	case msgs.Heartbeat:
+		a.hbMsgs = append(a.hbMsgs, msg)
+	case msgs.DataPlane, msgs.ControlPlane:
+		a.recMsgs = append(a.recMsgs, msg)
+	default:
+		return errors.New("unknown message type")
+	}
+	return nil
+}
+
+func (a *auditMsgCollector) CloseSend(ctx context.Context) error {
+	return nil
+}
+
+var validRecord = msgs.Record{
+	CallerIpAddress:            msgs.MustParseAddr("192.168.0.1"),
+	CallerIdentities:           map[msgs.CallerIdentityType][]msgs.CallerIdentityEntry{msgs.UPN: {{"user1@domain.com", "Description"}}},
+	OperationCategories:        []msgs.OperationCategory{msgs.UserManagement},
+	TargetResources:            map[string][]msgs.TargetResourceEntry{"ResourceType": {{"Name", "Cluster", "DataCenter", "Region"}}},
+	CallerAccessLevels:         []string{"Level1"},
+	OperationAccessLevel:       "AccessLevel",
+	OperationName:              "Operation",
+	OperationResultDescription: "ResultDescription",
+	CallerAgent:                "Agent",
+}
+
+// TestResetHappensOnClient is a practical test that shows that the client handles calling base.Client.Reset()
+// when an unrecoverable error is returned from base.Client.Send().
+func TestResetHappensOnClient(t *testing.T) {
+	t.Parallel()
+
+	numMsgs := 100000
+
+	msg := msgs.Msg{
+		Type:   msgs.DataPlane,
+		Record: validRecord.Clone(),
+	}
+
+	conn0 := &auditMsgCollector{errAfter: 1000}
+	conn1 := &auditMsgCollector{}
+
+	ccCalled := false
+	cc := func() (conn.Audit, error) {
+		if ccCalled {
+			return conn1, nil
+		}
+		ccCalled = true
+		return conn0, nil
+	}
+
+	client, err := New(cc, WithAuditOptions(base.WithSettings(base.Settings{QueueSize: 4000})))
+	if err != nil {
+		panic(err)
+	}
+
+	for i := 0; i < numMsgs; i++ {
+	tryAgain:
+		if err := client.Send(context.Background(), msg); err != nil {
+			if base.IsQueueFull(err) {
+				time.Sleep(100 * time.Millisecond)
+				goto tryAgain
+			}
+		}
+	}
+	client.Close(context.Background())
+
+	if len(conn0.recMsgs)+len(conn1.recMsgs) != numMsgs {
+		t.Errorf("Expected %d messages, but got %d", numMsgs, len(conn0.recMsgs)+len(conn1.recMsgs))
+	}
+}
